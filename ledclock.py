b81a8e23288477b4f8a31138b2bd28da0f079542
russ <russell.winch@gmail.com>
2017-10-07 22:08:47 +0100

    initial commit

---
+++ ledclock.py
@@ -0,0 +1,78 @@
+
+
+import time
+import ntptime
+import machine
+import ssd1306
+
+i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
+oled = ssd1306.SSD1306_I2C(64, 48, i2c) # width, height, pins
+
+spi = machine.SPI(1, baudrate=5000000, polarity=0, phase=0)
+cs = machine.Pin(15, machine.Pin.OUT)
+cs.on()
+
+def display(a,b,c,d):
+    # data register
+    dreg = bytearray(3)
+    dreg[0] = 0b00000000
+    dreg[1] = 0b00010001
+    dreg[2] = 0b00100011
+
+    cs.off()
+    spi.write(dreg)
+    cs.on()
+
+    # control register
+    creg = bytearray(1)
+    creg[0] = 0b11000001
+
+    cs.off()
+    spi.write(creg)
+    cs.on()
+
+def setTime():
+    global LASTUPDATE
+    global UPDATEINTERVAL
+    LASTUPDATE = time.time()
+    try:
+        ntptime.settime()
+        UPDATEINTERVAL = 300 #5mins
+        print('succesfully synced time from ntp server: ' + ntptime.host)
+        print('next update in ' + str(UPDATEINTERVAL) + ' seconds')
+        # return True
+    except OSError:
+        UPDATEINTERVAL = 10 #seconds
+        print('error, couldn\'t retrieve time')
+        print('trying again in ' + str(UPDATEINTERVAL) + ' seconds')
+        # return False
+
+def checkUpdate(last, interv):
+    if time.time() - (interv) > last:
+        return True
+    else:
+        return False
+
+def pad(p):
+    if p < 10:
+        return '0' + str(p)
+    else:
+        return str(p)
+
+def printTime(h, m, s):
+    print(time.localtime())
+    oled.fill(0) # clear the screen
+    oled.text(pad(h + 1) + ':' + pad(m) + ':' + pad(s), 0, 0) #hack for BST!!
+    oled.show()
+
+setTime()
+oldTime = time.time()
+
+while True:
+    if checkUpdate(LASTUPDATE, UPDATEINTERVAL) == True:
+        setTime()
+
+    if time.time() != oldTime:
+        oldTime = time.time()
+        # printTime(time.localtime()[3], time.localtime()[4], time.localtime()[5])
+        # printBinTime(time.localtime()[3], time.localtime()[4], time.localtime()[5])
