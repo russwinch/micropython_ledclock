8e6a46ceecade3dcc175b4f99e10933c0ce9f25d
russ <russell.winch@gmail.com>
2017-10-10 21:40:53 +0100

    tidied functions and added sync display

--- ledclock.py
+++ ledclock.py
@@ -7,15 +7,15 @@ def wifi_connect():
     sta_if = network.WLAN(network.STA_IF)
     try:
         with open("credentials.txt") as c:
-            u = c.readline()
-            p = c.readline()
+            uid = c.readline()
+            pasw = c.readline()
     except OSError:
         print('couldn\'t load credentials file')
     if not sta_if.isconnected():
         timeout = time.time() + 10 #seconds
-        print('connecting to network:', u)
+        print('connecting to network:', uid)
         sta_if.active(True)
-        sta_if.connect(u.strip(), p.strip()) # strip newlines
+        sta_if.connect(uid.strip(), pasw.strip()) # strip newlines
         while not sta_if.isconnected():
             countdown = timeout - time.time()
             if countdown > 0:
@@ -28,23 +28,43 @@ def wifi_connect():
         print('ip config:', sta_if.ifconfig())
         return True
 
-def display(a,b,c,d,h):
+def displayTime(h, m, s):
+    print(time.localtime())
+    a = int(h / 10)
+    b = h % 10
+    c = int(m / 10)
+    d = m % 10
+    h = (s % 2) * 112
+
     # data register
     dreg = bytearray(3)
-    dreg[0] = 0b10000000 | (h << 4) # if seconds are odd flash bank1 decimal
+    dreg[0] = 0b10000000 | h # if seconds are odd flash all decimals
     dreg[1] = (a << 4) + b # shift first nibble and add second to form a byte
     dreg[2] = (c << 4) + d
-    cs.off()
-    spi.write(dreg)
-    cs.on()
+    writeOut(dreg)
 
     # control register
     creg = bytearray(1)
     creg[0] = 0b11000001 # first 2 bits define special decode option.
     if a == 0:
         creg[0] |= 10000 # blank first digit with special decode
+    writeOut(creg)
+
+# displays the word 'sync'
+def displaySync():
+    dreg = bytearray(3)
+    dreg[0] = 0b10000000
+    dreg[1] = 0b01011100
+    dreg[2] = 0b01100001
+    writeOut(dreg)
+
+    creg = bytearray(1)
+    creg[0] = 0b11001111
+    writeOut(creg)
+
+def writeOut(self):
     cs.off()
-    spi.write(creg)
+    spi.write(self)
     cs.on()
 
 def setTime():
@@ -56,27 +76,12 @@ def setTime():
         UPDATEINTERVAL = 300 #5mins
         print('succesfully synced time from ntp server: ' + ntptime.host)
         print('next update in ' + str(UPDATEINTERVAL) + ' seconds')
-        # return True
+        return True
     except OSError:
         UPDATEINTERVAL = 10 #seconds
         print('error, couldn\'t retrieve time')
         print('trying again in ' + str(UPDATEINTERVAL) + ' seconds')
-        # return False
-
-def checkUpdate(last, interv):
-    if time.time() - (interv) > last:
-        return True
-    else:
         return False
-
-def printTime(h, m, s):
-    print(time.localtime())
-    h1 = int(h/10)
-    h2 = h%10
-    m1 = int(m/10)
-    m2 = m%10
-    s1 = s%2
-    display(h1,h2,m1,m2,s1)
 
 # initialise spi
 spi = machine.SPI(1, baudrate=5000000, polarity=0, phase=0)
@@ -85,14 +90,16 @@ cs.on()
 
 # connect to network
 wifi_connect() #** add retry functionality**
-setTime()
+while setTime() == False:
+    displaySync()
 oldTime = time.time()
 
 while True:
-    if checkUpdate(LASTUPDATE, UPDATEINTERVAL) == True:
+    # if checkUpdate(LASTUPDATE, UPDATEINTERVAL) == True:
+    if time.time() - (UPDATEINTERVAL) > LASTUPDATE:
         setTime()
 
     if time.time() != oldTime:
         oldTime = time.time()
-        printTime(time.localtime()[3]+1, time.localtime()[4],
+        displayTime(time.localtime()[3]+1, time.localtime()[4],
                 time.localtime()[5]) #BST hack for hour!
