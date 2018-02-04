41995b71ebcfbaac7fde7f5da7ddcceda211e967
russ <russell.winch@gmail.com>
2018-02-04 21:39:06 +0000

    added dipswitch class. tidied up functions and added main(). improved documentation in docstrings

--- ledclock.py
+++ ledclock.py
@@ -1,8 +1,9 @@
 """
-led clock
-a ntp synching clock in micropython
+led clock.
+
+An NTP synching clock in micropython
 @author Russ Winch
-@version December 2017
+@version January 2018 
 """
 
 import time
@@ -11,45 +12,35 @@ import ntptime
 from machine import Pin, SPI
 from wifi import Wifi
 
-def setTime():
-    success_update = 300 # 5 mins
-    fail_update = 10 # seconds
-    last_update = time.time()
-    try:
-        ntptime.settime()
-        update_interval = success_update
-        print("succesfully synced time from ntp server: " + ntptime.host)
-        print("next update in " + str(update_interval) + " seconds")
-    except OSError:
-        update_interval = fail_update
-        print("error, couldn't retrieve time")
-        print("trying again in " + str(update_interval) + " seconds")
-    finally:
-        return last_update, update_interval
 
-# class DipSwitch:
-#     '''controls dipswitches. not working yet but should enable easy addition'''
-#     def __init__(self, switchPin):
-#        self = Pin(switchPin, Pin.IN, Pin.PULL_UP)
+class DipSwitch:
+    """controls dipswitches."""
 
-    # def val(self):
-    #     return 1 - self.Value()
+    def __init__(self, switchPin):
+        """initialise a dipswitch on the defined pin"""
+        self.switch = Pin(switchPin, Pin.IN, Pin.PULL_UP)
+
+    def value(self):
+        return 1 - self.switch.value()
+
 
 class SevenSeg(object):
     """control of seven segment displays via MC14489B LED driver"""
+
     def __init__(self, csPin):
-        # initialise spi
+        """initialise the SPI bus""" 
         self.spi = SPI(1, baudrate=5000000, polarity=0, phase=0)
         self.cs = Pin(csPin, Pin.OUT) # chip select
         self.cs.on()
 
     def writeOut(self, data):
+        """sends data to the display over the SPI bus"""
         self.cs.off()
         self.spi.write(data)
         self.cs.on()
 
     def printSync(self):
-        # displays the word 'sync'
+        """displays the word 'Sync'"""
         dreg = bytearray(3)
         dreg[0] = 0b10000000
         dreg[1] = 0b01011100
@@ -61,7 +52,7 @@ class SevenSeg(object):
         self.writeOut(creg)
 
     def printConn(self):
-        # displays the word 'Conn'
+        """displays the word 'Conn'"""
         dreg = bytearray(3)
         dreg[0] = 0b10000000
         dreg[1] = 0b11000111
@@ -72,8 +63,12 @@ class SevenSeg(object):
         creg[0] = 0b11001111
         self.writeOut(creg)
 
-    def printTime(self, h, m, s):
+    def printTime(self, h, m, s, dips=None):
+        """prints the time to the display"""
         print(time.localtime())
+
+        for d, _ in enumerate(dips):
+            print(dips[d].value())
         # print(time.time())
         # print(ntptime.time())
         # if h > 12 and dip3.value() == 1:
@@ -97,22 +92,42 @@ class SevenSeg(object):
         # control register
         creg = bytearray(1)
         creg[0] = 0b11000001 # first 2 bits define special decode option.
-        if a == 0 and dip1.value() == 1:
+        if a == 0: # and dip1.value() == 1:
             creg[0] |= 10000 # blank first digit with special decode
         self.writeOut(creg)
 
-if __name__ == "__main__":
+
+def setTime():
+    """Retrieves the time from NTP server."""
+    success_update = 300 # 5 mins
+    fail_update = 10 # seconds
+    last_update = time.time()
+
+    try:
+        ntptime.settime()
+        update_interval = success_update
+        print("succesfully synced time from ntp server: " + ntptime.host)
+        print("next update in " + str(update_interval) + " seconds")
+    except OSError as e:
+        update_interval = fail_update
+        print(e)
+        print("error, couldn't retrieve time")
+        print("trying again in " + str(update_interval) + " seconds")
+    finally:
+        return last_update, update_interval
+
+
+def main():
     display = SevenSeg(15) # init display with GPIO15 as the CS pin
 
-    dip1 = Pin(5, Pin.IN, Pin.PULL_UP) # D1
-    dip2 = Pin(4, Pin.IN, Pin.PULL_UP) # D2
-    dip3 = Pin(16, Pin.IN) # D0
+    # dip1 = Pin(5, Pin.IN, Pin.PULL_UP) # D1
+    # dip2 = Pin(4, Pin.IN, Pin.PULL_UP) # D2
+    # dip3 = Pin(16, Pin.IN) # D0
 
-    # non working dipswitch class - to be continued
-    # switchPins = [5,4]
-    # dip = list()
-    # for i in range(len(switchPins)):
-    #     dip.append(DipSwitch(switchPins[i]))
+    switchPins = [5,4]
+    dips = []
+    for i, _ in enumerate(switchPins):
+        dips.append(DipSwitch(switchPins[i]))
 
     # connect to network
     display.printConn()
@@ -136,5 +151,12 @@ if __name__ == "__main__":
             # check if display needs updating
             if time.time() != oldTime:
                 oldTime = time.time()
-                display.printTime(time.localtime()[3], time.localtime()[4],
-                        time.localtime()[5])
+                display.printTime(
+                        h = time.localtime()[3],
+                        m = time.localtime()[4],
+                        s = time.localtime()[5],
+                        dips = dips)
+
+
+if __name__ == "__main__":
+    main()
