675a5cfeb7bb488dbe5941fc20b45828002375b5
russ <russell.winch@gmail.com>
2018-02-16 00:39:26 +0000

    implemented RTC module. added DST functionality. improved clarity of variables within SevenSeg class

--- ledclock.py
+++ ledclock.py
@@ -9,9 +9,9 @@ An NTP synching clock in micropython
 import time
 # import network
 import ntptime
-from machine import Pin, SPI
+from machine import I2C, Pin, SPI
 from wifi import Wifi
-
+from rtc import RTC
 
 class DipSwitch(object):
     """
@@ -31,10 +31,10 @@ class DipSwitch(object):
 class SevenSeg(object):
     """control of seven segment displays via MC14489B LED driver"""
 
-    def __init__(self, csPin):
+    def __init__(self, chip_select_pin, spi):
         """initialise the SPI bus""" 
-        self.spi = SPI(1, baudrate=5000000, polarity=0, phase=0)
-        self.cs = Pin(csPin, Pin.OUT) # chip select
+        self.spi = spi
+        self.cs = Pin(chip_select_pin, Pin.OUT) # chip select
         self.cs.on()
 
     def writeOut(self, data):
@@ -67,42 +67,41 @@ class SevenSeg(object):
         creg[0] = 0b11001111
         self.writeOut(creg)
 
-    def printTime(self, h, m, s, dips=None):
+    def printTime(self, hour, minute, second, dips=None):
         """prints the time to the display"""
         print("----------")
-        print("{hrs}:{mins}:{secs}".format(hrs=h, mins=m, secs=s))
+        print("{h}:{m}:{s}".format(h=hour, m=minute, s=second))
 
         if dips:
             for d, _ in enumerate(dips):
                 print("dipswitch {d}: {val}".format(d=d, val=dips[d].value()))
 
-        # if h > 12 and dip3.value() == 1:
-        #     h -= 12 # 12 hour mode
-        a = int(h / 10)
-        b = h % 10
-        c = int(m / 10)
-        d = m % 10
+        # break digits down into led banks
+        bank_a = int(hour / 10)
+        bank_b = hour % 10
+        bank_c = int(minute / 10)
+        bank_d = minute % 10
         # if dip2.value() == 1:
-        h = (s % 2) # flashing separator
+        bank_h = (second % 2) # flashing separator
         # else:
         #     h = 1 # static separator
 
         # data register
         dreg = bytearray(3)
-        dreg[0] = 0b10000000 | h * 112 # if seconds are odd flash all decimals
-        dreg[1] = (a << 4) + b # shift first nibble and add second
-        dreg[2] = (c << 4) + d
+        dreg[0] = 0b10000000 | bank_h * 112 # when seconds odd flash decimals
+        dreg[1] = (bank_a << 4) + bank_b # shift first nibble and add second
+        dreg[2] = (bank_c << 4) + bank_d # shift first nibble and add second
         self.writeOut(dreg)
 
         # control register
         creg = bytearray(1)
         creg[0] = 0b11000001 # first 2 bits define special decode option.
-        if a == 0  and dips[1].value() == 1:
+        if bank_a == 0 and dips[1].value() == 1:
             creg[0] |= 10000 # blank first digit with special decode
         self.writeOut(creg)
 
 
-def setTime():
+def retrieve_ntp_time():
     """Retrieves the time from NTP server."""
     success_update = 300 # 5 mins
     fail_update = 10 # seconds
@@ -122,19 +121,25 @@ def setTime():
         return last_update, update_interval
 
 
-# function to set RTC
+def dst(hour, apply_dst, time_diff):
+    """Determines if DST dipswitch is set and if so applies time difference"""
+    if apply_dst:
+        hour += time_diff
+        if hour > 23:
+            hour -= 24 
+    return hour
 
-# function to convert from DEC to BCD
-
-# function to convert from BCD to DEC
-
-# function to retrieve the time from the RTC
 
 # function to log the retrieved data and any errors from the retreived time
 #load this from external file...
 
 def main():
-    display = SevenSeg(15) # init display with GPIO15 as the CS pin
+    # initialise spi bus and i2c bus
+    spi = SPI(1, baudrate=5000000, polarity=0, phase=0)
+    i2c = I2C(scl=Pin(16), sda=Pin(0), freq=100000)
+
+    display = SevenSeg(15, spi) # init display with CS pin GPIO15 on spi bus
+    rtc = RTC(i2c) # init rtc on i2c bus
 
     switchPins = [5,4] # D1, D2
     dips = []
@@ -150,23 +155,29 @@ def main():
 
     display.printSync()
     # issue here needs resolving as failure to retrive time results in 00:00
-    last_update, update_interval = setTime()
-    oldTime = time.time()
+    last_update, update_interval = retrieve_ntp_time()
+    rtc.set_time(time.localtime()[3:6])
+    old_time = rtc.get_time() 
 
     while True:
         # check if an update is due
         if (time.time() - update_interval) > last_update:
-            last_update, update_interval = setTime()
+            # add check for online here
+            last_update, update_interval = retrieve_ntp_time()
+            rtc.set_time(time.localtime()[3:6])
 
+        rtc_time = rtc.get_time()
         # check if display needs updating
-        if time.time() != oldTime:
-            oldTime = time.time()
-            # print(time.localtime())
+        if rtc_time != old_time:
+            old_time = rtc_time
             display.printTime(
-                    h = time.localtime()[3],
-                    m = time.localtime()[4],
-                    s = time.localtime()[5],
-                    dips = dips)
+                    hour=dst(
+                        hour=rtc_time.hour, 
+                        apply_dst=dips[0].value(),
+                        time_diff=1),
+                    minute=rtc_time.minute,
+                    second=rtc_time.second,
+                    dips=dips)
 
 
 if __name__ == "__main__":
--- rtc.py
+++ rtc.py
@@ -1,52 +1,88 @@
 """RTC time control for DS1307.
 
-Functionality to utilise the RTC over i2c
+Utilise the time functionality of the RTC over i2c.
+This only includes the hours, minutes and seconds. 
+Based on the uRTC module from Adafruit
+https://github.com/adafruit/Adafruit-uRTC
 
-@version 14-Feb-2018
+@version 0.2 - February 2018
 @author Russ Winch (github.com/russwinch)
 """
 
-import time
-from machine import I2C, Pin
+# import time # for testing only
+import ucollections
+# from machine import I2C, Pin # for testing only
 
-# i2c = I2C(scl=Pin(2), sda=Pin(0), freq=100000)
-i2c = I2C(scl=Pin(16), sda=Pin(0), freq=100000)
+# for testing only, move our of here and pass in these variables
+# i2c = I2C(scl=Pin(16), sda=Pin(0), freq=100000)
+# address = 0x68
 
-# mem = i2c.scan()
-mem = 0x68
-# print(mem[0])
+TimeTuple = ucollections.namedtuple("TimeTuple", ["hour", "minute", "second"])
+
+def timetuple(hour=0, minute=0, second=0):
+    """creates a TimeTuple named tuple object"""
+    return TimeTuple(hour, minute, second)
 
 def bcd2bin(value):
+    """Converts from Binary Coded Decimal"""
     return (value or 0) - 6 * ((value or 0) >> 4)
 
 def bin2bcd(value):
+    """Converts to Binary Coded Decimal"""
     return (value or 0) + 6 * ((value or 0) // 10)
 
-def start(mem):
-    i2c.writeto_mem(mem, 0, b'0')
 
-def read(mem):
-    return i2c.readfrom_mem(mem, 0, 3)
+class RTC(object):
 
-def set(mem, newtime):
-    buffer = bytearray(3)
-    buffer[0] = bin2bcd(newtime[2])
-    buffer[1] = bin2bcd(newtime[1])
-    buffer[2] = bin2bcd(newtime[0])
-    i2c.writeto_mem(mem, 0, buffer)
+    def __init__(self, i2c, address=0x68):
+        """Initialise with the i2c object and optionally the i2c address"""
+        self.i2c = i2c
+        self.address = address
 
-# start(mem[0])
-set(mem, (22,20,00))
+    def start_clock(self):
+        """Start the clock by resetting the seconds to zero, for testing"""
+        buffer = bytearray(1)
+        buffer[0] = 0
+        self.i2c.writeto_mem(self.address, 0, buffer)
 
-old_buffer = 0
+    def get_time(self):
+        """Retrieve 3 bytes of time from the RTC and return as a timetuple"""
+        buffer =  self.i2c.readfrom_mem(self.address, 0, 3)
+        return timetuple(
+                hour=bcd2bin(buffer[2]),
+                minute=bcd2bin(buffer[1]),
+                second=bcd2bin(buffer[0]))
 
-while True:
-    buffer = read(mem)
-    if buffer != old_buffer:
-            print("{h}:{m}:{s}".format(
-            h=bcd2bin(buffer[2]), 
-            m=bcd2bin(buffer[1]), 
-            s=bcd2bin(buffer[0])))
-            print('-------')
-            old_buffer = buffer
-    # time.sleep(1)
+    def set_time(self, newtime):
+        """Write 3 bytes of time to the RTC"""
+        newtime = timetuple(*newtime)
+        buffer = bytearray(3)
+        buffer[0] = bin2bcd(newtime.second)
+        buffer[1] = bin2bcd(newtime.minute)
+        buffer[2] = bin2bcd(newtime.hour)
+        # buffer[0] = bin2bcd(newtime[2])
+        # buffer[1] = bin2bcd(newtime[1])
+        # buffer[2] = bin2bcd(newtime[0])
+        self.i2c.writeto_mem(self.address, 0, buffer)
+
+if __name__ == "__main__":
+    # start(address)
+    # set(address, (8,17,37))
+
+    # old_buffer = 0
+    rtc = RTC(i2c)
+    new_time = timetuple(hour=21, minute=5, second=35)
+    rtc.set_time(new_time)
+    # rtc.set_time((22, 30, 45))
+
+    while True:
+        # buffer = read(address)
+        # if buffer != old_buffer:
+        #         print("{h}:{m}:{s}".format(
+        #         h=bcd2bin(buffer[2]), 
+        #         m=bcd2bin(buffer[1]), 
+        #         s=bcd2bin(buffer[0])))
+        #         print('-------')
+        #         old_buffer = buffer
+        print(rtc.get_time())
+        time.sleep(1)
